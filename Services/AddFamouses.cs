58fed842b768862adb85675fc4d2a975697f7beb
celilhn <ckadioglu@amvg.com>
2022-04-03 03:01:30 +0300

    Add Service

---
+++ Services/AddFamouses.cs
@@ -0,0 +1,36 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AddFamouses
+    {
+        static public void GetHtml()
+        {
+            int counterofAdded = 0;
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("https://tr.wikipedia.org/wiki/T%C3%BCrk_oyuncular_listesi");
+            for (int i = 0; i < 28; i++)
+            {
+                Boolean isExist = true;
+                string xPath = "//*[@id='mw-content-text']/div[1]/ul[" + (i + 1) + "]";
+                HtmlNode ul = document.DocumentNode.SelectNodes(xPath).First();
+                HtmlNode[] li = ul.SelectNodes("li").ToArray();
+                foreach (HtmlNode liNode in li)
+                {
+                    //isExist = ControlisFamousExist((liNode.InnerText).ToString());
+                    if (!isExist)
+                    {
+                        //AddFamous((liNode.InnerText).ToString());
+                        counterofAdded++;
+                    }
+                }
+            }
+            Console.WriteLine(counterofAdded + " Adet kişi Eklendi.");
+        }
+    }
+}
---
+++ Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Services
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AddFamouses addFamouses = new AddFamouses();
+        }
+    }
+}
