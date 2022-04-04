2897995aaf40dcae05bc2d16570843a87895e7bc
Celilhan Kadıoğlu <ckadioglu@amvg.com>
2022-04-04 11:48:06 +0300

    Add Award Tables

---
+++ Domain/Models/AwardCategory.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Models
+{
+    [Table("AwardCategories")]
+    public class AwardCategory : ExtendedBaseModel
+    {
+        [Column(TypeName = "varchar(100)")]
+        public string CategoryName { get; set; }
+    }
+}
